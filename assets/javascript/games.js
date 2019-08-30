e97d826e65f2747ce509c20ba52b5e195782b665
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-08-29 23:43:59 -0700

    added 2 hints to the game play cycle.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,6 +3,7 @@ var wins = 0;
 var losses = 0;
 var guesses = 10;
 var alphaRand = 0;
+var hintsGiven = 0;
 
 // alphabet is entered as a string and converts it to an array
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
@@ -11,7 +12,7 @@ var guessMade = [];
 // initial output values to screen
 function scoreBoard() {
   var guessMadeString = guessMade.join(', '); //adding commas
-  
+
   document.querySelector("#wins").innerHTML = `Wins: ${wins}`;
   document.querySelector("#losses").innerHTML = `Losses: ${losses}`;
   document.querySelector("#guess-remaining").innerHTML = `Guesses Remaining: ${guesses}`;
@@ -26,6 +27,51 @@ function pick() {
     Math.floor(Math.random() * alphabet.length)
   ];
   console.log('picked letter: ' + alphaRand);
+}
+
+function resetGame() {
+  wins = 0;
+  losses = 0;
+  guesses = 10;
+  guessMade = [];
+  hintsGiven = 0;
+  document.querySelector('#hint1').innerHTML = '';
+  document.querySelector('#hint2').innerHTML = '';
+}
+
+function hint1() {
+  if (alphabet.indexOf(alphaRand) < 13) {
+    document.querySelector('#hint1').innerHTML = 
+      'Hint 1: The letter I picked is in the first half of the alphabet!';
+  } else if (alphabet.indexOf(alphaRand) >= 13) {
+    document.querySelector('#hint1').innerHTML = 
+    'Hint 1: The letter I picked is in the second half of the alphabet!';
+  }
+  
+}
+
+function hint2() {
+  var vowls = ['a', 'e', 'i', 'o', 'u']
+  if (vowls.includes(alphaRand)) {
+    document.querySelector('#hint2').innerHTML = 
+      'Hint 2: The letter I picked is a vowl.';
+      console.log('vowl')
+  } else {
+    document.querySelector('#hint2').innerHTML = 
+      'Hint 2: The letter I picked is a consonant.';
+      console.log('consonant')
+  }
+}
+function hints() {
+  if (hintsGiven === 0) {
+    hint1();
+    hintsGiven++;
+  } else if (hintsGiven === 1) {
+    hint2();
+    hintsGiven++;
+  } else {
+    alert("Hey buddy, I think I've given you enough hints!")
+  }
 }
 
 pick();
@@ -67,10 +113,7 @@ document.onkeyup = function(event) {
   if (wins === 10) {
     scoreBoard();
     if (confirm('You ARE psychic, wow! Would you like to play again?')) {
-      wins = 0;
-      losses = 0;
-      guesses = 10;
-      guessMade = [];
+      resetGame();
       scoreBoard();
     } else {
       alert("I knew psychics were fake...");
@@ -79,10 +122,7 @@ document.onkeyup = function(event) {
   }
   if (losses === 10) {
     if (confirm('Nice try, friend. Would you like a rematch?')) {
-      wins = 0;
-      losses = 0;
-      guesses = 10;
-      guessMade = [];
+      resetGame();
       scoreBoard();
     } else {
       alert("I'll get you next time...");
